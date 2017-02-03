e50494dfda1c651fb92f4fd00b1877cc1e205997
Nahid <nahidshaiket10300@gmail.com>
2017-02-03 16:02:57 +0600

    heroku problem

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 
 var About = (props) => {
-  return (<h3>Welcome to About!</h3>);
+  return (<h3>Welcome to About!!!</h3>);
 };
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 
 var Example = (props) => {
-  return (<h3>Welcome to Example</h3>);
+  return (<h3>Welcome to Example!!!</h3>);
 };
 
 module.exports = Example;
