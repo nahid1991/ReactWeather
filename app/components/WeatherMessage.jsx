15a0f31a2611b79554ace9575e5d4e5ba8b4333e
Nahid <nahidshaiket10300@gmail.com>
2017-02-04 11:54:41 +0600

    tweaking

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,9 @@ var React = require('react');
 
 var WeatherMessage = ({temp,location}) => {
   return (
-      <h1 class="text-center">It is {temp}&#x2103; in {location}</h1>
+    <div>
+      <h3 className="text-center">It is {temp}&#x2103; in {location}</h3>
+    </div>
     );
 }
 
