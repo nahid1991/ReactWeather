4a9b592139b3a8903e17c3d7ea052ca512095623
Nahid <nahidshaiket10300@gmail.com>
2017-02-04 11:47:58 +0600

    done with weather app

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -17,7 +17,9 @@ var Weather = React.createClass({
     // debugger;
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(function(temp){
@@ -32,6 +34,23 @@ var Weather = React.createClass({
         errorMessage: errorMessage.message
       });
     });
+  },
+
+  componentDidMount: function(){
+    var location = this.props.location.query.location;
+
+    if(location && location.length > 0){
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function(newProps){
+    var location = newProps.location.query.location;
+
+    if(location && location.length > 0){
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function(){
     var {isLoading, temp, location, errorMessage} = this.state;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,7 @@ var React = require('react');
 var WeatherMessage = ({temp,location}) => {
   return (
     <div>
-      <h3>It is {temp}&#x2103; in {location}</h3>
+      <h1 class="text-center">It is {temp}&#x2103; in {location}</h1>
     </div>);
 }
 
