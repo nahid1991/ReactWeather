b2678c7880732ed69ae92bb395a2eb36f5ecc3c7
Nahid <nahidshaiket10300@gmail.com>
2017-02-05 13:55:26 +0600

    tweaking

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,7 @@ var About = (props) => {
           <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework I used.
         </li>
         <li>
-          <a href="http://openweathermap.org">Oper Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
         </li>
       </ul>
     </div>);
